fa83b9375dd0eb15d415318e09e856e8846573f0
Tang Rui <talltangrui@outlook.com>
2016-10-10 23:14:32 +0800

    刚开始写词法分析器

---
+++ Compiler/src/Lexical/word_table.java
@@ -0,0 +1,11 @@
+package Lexical;
+
+public class word_table {
+	public static String key[] = {"auto", "break", "case", "char", "const"
+	, "continue", "default", "do", "double", "else", "enum", "extern", 
+	"float", "for", "goto", "if", "int", "long", "register", "return",
+	"short", "signed", "static", "sizeof", "struct", "switch", "typeof",
+	"union", "unsigned", "void", "volatile", "while"};
+	
+	
+}
